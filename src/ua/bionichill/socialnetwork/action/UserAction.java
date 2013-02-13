f3c4a9c9868bc0137b5076161abbde18a883d40c
baranovRP <baranov.r.p@gmail.com>
2013-02-13 21:15:41 +0200

    add user actions

---
+++ src/ua/bionichill/socialnetwork/action/UserAction.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package ua.bionichill.socialnetwork.action;
+
+import java.util.List;
+
+import ua.bionichill.socialnetwork.dto.User;
+
+/**
+ * @author Roman Baranov
+ * 
+ */
+public interface UserAction {
+
+    /**
+     * Create a new user (registration).
+     */
+    public User createUser(User user);
+
+    /**
+     * Updates an information about user.
+     */
+    public User updateUser(String email, User user);
+
+    /**
+     * Deletes a user.
+     */
+    public void delete(String email);
+
+    /**
+     * Get a user by PK (login?).
+     */
+    public User getUserByPK(String email);
+
+    /**
+     * Get a list of all users.
+     */
+    public List<User> getUserList();
+
+    /**
+     * Block a user.
+     */
+    public void blockUser(String email);
+}
